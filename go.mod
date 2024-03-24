93a2ca27de7f9aa9b8019ebda22990d1b4176908
Or Geva <orgab@jfrog.com>
2024-03-24 12:48:21 +0200

    second commit

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/or-geva/test-private
 
 go 1.22.1
+
+require github.com/jfrog/jfrog-desktop-server v0.0.0-20240321133751-ca91dac5ff5b // indirect
--- main.go
+++ main.go
@@ -1,5 +1,9 @@
 package main
 
+import "github.com/jfrog/jfrog-desktop-server/pkg/api"
+
 func main() {
-	println("Hello, World!")
+	var x api.FilesDownloadRequest
+	x.Checksums = append(x.Checksums, "1234")
+	println("Hello, World!" + x.Checksums[0])
 }
